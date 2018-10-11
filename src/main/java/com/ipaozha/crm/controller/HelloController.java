ea315f4edf452f4a656b27b8a06099d381d434ad
suyikun <270081841@qq.com>
2018-10-11 16:15:07 +0800

    hello

---
+++ src/main/java/com/ipaozha/crm/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.ipaozha.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/api/hello")
+    @ResponseBody
+    public String apiHello() {
+        return "{'a':'hello'}";
+    }
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/main/resources/templates/hello.ftl
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>hello页面</title>
+</head>
+<body>
+<h1>hello</h1>
+<h2>hello</h2>
+<h3>hello</h3>
+</body>
+</html>
